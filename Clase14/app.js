ad46d944f62856f011d0754c84d6fe5abfcdc328
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2022-03-29 17:57:10 -0300

    subo archivos para clase 15

--- Clase14/app.js
+++ Clase14/app.js
@@ -7,3 +7,9 @@ app.listen(3000,()=>console.log("Running at 3000 port"));
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,("./views/home.html")))
 })
+app.get('/login.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,("./views/login.html")))
+})
+app.get('/register.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,("./views/register.html")))
+})
