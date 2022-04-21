dbee0748f1f772fd31093d3043d7ebef9cb9649d
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2022-04-21 16:17:29 -0300

    subiendo clase 21

---
+++ Clase21/controllers/userController.js
@@ -0,0 +1,28 @@
+let users=[ {id:1, nombre:"David"},
+            {id:2, nombre:"Leandro"},
+            {id:3, nombre:"Alba"},
+            {id:4, nombre:"Araceli"},
+            {id:4, nombre:"Martin"},
+            {id:4, nombre:"Jasmin"},
+            {id:5, nombre:"Campo de Verificacion =)"}]
+
+const userController = {
+    register: (req,res)=>{
+        res.render('./users/register');
+    },
+    login: (req,res)=>{
+        res.render('./users/login');
+    },
+    list:(req,res)=>{
+        res.render('./users/userList',{users: users});
+    },
+    search:(req,res)=>{
+        let search = req.query.search;
+        let userResults = users.filter((usuario)=>{return usuario.nombre.includes(search)})
+        res.render('./users/userResults',{userResults:userResults})
+    },
+    create:(req,res)=>{res.send(req.body)}
+
+}
+
+module.exports = userController;
---
+++ Clase21/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController.js')
+
+
+router.get('/',(req,res)=>{
+    res,send("respond with a resource");
+});
+
+router.get('/login',userController.login)
+router.get('/register',userController.register)
+router.post('/register',userController.create)
+router.get('/list',userController.list)
+router.get('/search/',userController.search)
+
+module.exports = router;
